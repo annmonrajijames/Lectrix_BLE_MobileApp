c49dd0f4dd37ca6e20fa86bf0b5c618bdf7c908c
Annmon James <annmon.james@lectrixtech.com>
2024-08-20 10:39:59 +0530

    New page added between App.tsx(Bluetooth page) and DataTransfer.tsx page, this page will help user decide to choose from where the data should be send or received (VCU or Mobile App)

---
+++ DataDirection.tsx
@@ -0,0 +1,41 @@
+// DataDirection.tsx
+import React from 'react';
+import { View, Button, StyleSheet, Text } from 'react-native';
+import { Device } from 'react-native-ble-plx';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  DataTransfer: { device: Device };
+  DataDirection: { device: Device };
+};
+
+type DataDirectionProps = NativeStackScreenProps<RootStackParamList, 'DataDirection'>;
+
+const DataDirection: React.FC<DataDirectionProps> = ({ route, navigation }) => {
+  const { device } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Text>Select Data Direction</Text>
+      <Button
+        title="Mobile app to VCU"
+        onPress={() => navigation.navigate('DataTransfer', { device })}
+      />
+      <Button
+        title="VCU to Mobile app"
+        onPress={() => console.log('VCU to Mobile app pressed')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+});
+
+export default DataDirection;
