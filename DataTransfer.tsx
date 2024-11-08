6df3b5eeafe90b7c72662e03239a43aa5add29d5
Annmon James <annmonjameschiramel@gmail.com>
2024-11-08 12:11:26 +0530

    Completed MaxCellVol, MinCellVol, AvgCellVol, MaxVoltId, MinVoltId

--- DataTransfer.tsx
+++ DataTransfer.tsx
@@ -29,6 +29,12 @@ const DataTransfer: React.FC<DataTransferProps> = ({ route }) => {
   const [cellVol15, setCellVol15] = useState<number | null>(null);
   const [cellVol16, setCellVol16] = useState<number | null>(null);
 
+  const [MaxCellVol, setMaxCellVol] = useState<number | null>(null);
+  const [MinCellVol, setMinCellVol] = useState<number | null>(null);
+  const [AvgCellVol, setAvgCellVol] = useState<number | null>(null);
+  const [MaxVoltId, setMaxVoltId] = useState<number | null>(null);
+  const [MinVoltId, setMinVoltId] = useState<number | null>(null);
+
   const serviceUUID = '00FF';
   const characteristicUUID = 'FF01';
 
@@ -91,6 +97,12 @@ const DataTransfer: React.FC<DataTransferProps> = ({ route }) => {
     const cellVoltage15 = eight_bytes_decode('08', 0.0001, 16, 17)(data);
     const cellVoltage16 = eight_bytes_decode('08', 0.0001, 18, 19)(data);
 
+    const MaxCellVol = eight_bytes_decode('09', 0.001, 1, 2)(data);
+    const MinCellVol = eight_bytes_decode('09', 0.001, 3, 4)(data);
+    const AvgCellVol = eight_bytes_decode('09', 0.001, 5, 6)(data);
+    const MaxVoltId = eight_bytes_decode('09', 1, 7)(data);
+    const MinVoltId = eight_bytes_decode('09', 1, 8)(data);
+
     if (cellVoltage01 !== null) setCellVol01(cellVoltage01);
     if (cellVoltage02 !== null) setCellVol02(cellVoltage02);
     if (cellVoltage03 !== null) setCellVol03(cellVoltage03);
@@ -107,31 +119,45 @@ const DataTransfer: React.FC<DataTransferProps> = ({ route }) => {
     if (cellVoltage14 !== null) setCellVol14(cellVoltage14);
     if (cellVoltage15 !== null) setCellVol15(cellVoltage15);
     if (cellVoltage16 !== null) setCellVol16(cellVoltage16);
+
+    if (MaxCellVol !== null) setMaxCellVol(MaxCellVol);
+    if (MinCellVol !== null) setMinCellVol(MinCellVol);
+    if (AvgCellVol !== null) setAvgCellVol(AvgCellVol);
+    if (MaxVoltId !== null) setMaxVoltId(MaxVoltId);
+    if (MinVoltId !== null) setMinVoltId(MinVoltId);
   };
 
   return (
     <ScrollView style={styles.scrollView}>
       <View style={styles.container}>
-        {cellVol01 !== null && <Text style={styles.cellVolText}>Cell Voltage 01: {cellVol01.toFixed(4)} V</Text>}
-        {cellVol02 !== null && <Text style={styles.cellVolText}>Cell Voltage 02: {cellVol02.toFixed(4)} V</Text>}
-        {cellVol03 !== null && <Text style={styles.cellVolText}>Cell Voltage 03: {cellVol03.toFixed(4)} V</Text>}
-        {cellVol04 !== null && <Text style={styles.cellVolText}>Cell Voltage 04: {cellVol04.toFixed(4)} V</Text>}
-        {cellVol05 !== null && <Text style={styles.cellVolText}>Cell Voltage 05: {cellVol05.toFixed(4)} V</Text>}
-        {cellVol06 !== null && <Text style={styles.cellVolText}>Cell Voltage 06: {cellVol06.toFixed(4)} V</Text>}
-        {cellVol07 !== null && <Text style={styles.cellVolText}>Cell Voltage 07: {cellVol07.toFixed(4)} V</Text>}
-        {cellVol08 !== null && <Text style={styles.cellVolText}>Cell Voltage 08: {cellVol08.toFixed(4)} V</Text>}
-        {cellVol09 !== null && <Text style={styles.cellVolText}>Cell Voltage 09: {cellVol09.toFixed(4)} V</Text>}
-        {cellVol10 !== null && <Text style={styles.cellVolText}>Cell Voltage 10: {cellVol10.toFixed(4)} V</Text>}
-        {cellVol11 !== null && <Text style={styles.cellVolText}>Cell Voltage 11: {cellVol11.toFixed(4)} V</Text>}
-        {cellVol12 !== null && <Text style={styles.cellVolText}>Cell Voltage 12: {cellVol12.toFixed(4)} V</Text>}
-        {cellVol13 !== null && <Text style={styles.cellVolText}>Cell Voltage 13: {cellVol13.toFixed(4)} V</Text>}
-        {cellVol14 !== null && <Text style={styles.cellVolText}>Cell Voltage 14: {cellVol14.toFixed(4)} V</Text>}
-        {cellVol15 !== null && <Text style={styles.cellVolText}>Cell Voltage 15: {cellVol15.toFixed(4)} V</Text>}
-        {cellVol16 !== null && <Text style={styles.cellVolText}>Cell Voltage 16: {cellVol16.toFixed(4)} V</Text>}
+        {cellVol01 !== null && <Text style={styles.parameterText}>Cell Voltage 01: {cellVol01.toFixed(4)} V</Text>}
+        {cellVol02 !== null && <Text style={styles.parameterText}>Cell Voltage 02: {cellVol02.toFixed(4)} V</Text>}
+        {cellVol03 !== null && <Text style={styles.parameterText}>Cell Voltage 03: {cellVol03.toFixed(4)} V</Text>}
+        {cellVol04 !== null && <Text style={styles.parameterText}>Cell Voltage 04: {cellVol04.toFixed(4)} V</Text>}
+        {cellVol05 !== null && <Text style={styles.parameterText}>Cell Voltage 05: {cellVol05.toFixed(4)} V</Text>}
+        {cellVol06 !== null && <Text style={styles.parameterText}>Cell Voltage 06: {cellVol06.toFixed(4)} V</Text>}
+        {cellVol07 !== null && <Text style={styles.parameterText}>Cell Voltage 07: {cellVol07.toFixed(4)} V</Text>}
+        {cellVol08 !== null && <Text style={styles.parameterText}>Cell Voltage 08: {cellVol08.toFixed(4)} V</Text>}
+        {cellVol09 !== null && <Text style={styles.parameterText}>Cell Voltage 09: {cellVol09.toFixed(4)} V</Text>}
+        {cellVol10 !== null && <Text style={styles.parameterText}>Cell Voltage 10: {cellVol10.toFixed(4)} V</Text>}
+        {cellVol11 !== null && <Text style={styles.parameterText}>Cell Voltage 11: {cellVol11.toFixed(4)} V</Text>}
+        {cellVol12 !== null && <Text style={styles.parameterText}>Cell Voltage 12: {cellVol12.toFixed(4)} V</Text>}
+        {cellVol13 !== null && <Text style={styles.parameterText}>Cell Voltage 13: {cellVol13.toFixed(4)} V</Text>}
+        {cellVol14 !== null && <Text style={styles.parameterText}>Cell Voltage 14: {cellVol14.toFixed(4)} V</Text>}
+        {cellVol15 !== null && <Text style={styles.parameterText}>Cell Voltage 15: {cellVol15.toFixed(4)} V</Text>}
+        {cellVol16 !== null && <Text style={styles.parameterText}>Cell Voltage 16: {cellVol16.toFixed(4)} V</Text>}
+
+        {MaxCellVol !== null && <Text style={styles.parameterText}>MaxCellVol: {MaxCellVol.toFixed(4)} V</Text>}
+        {MinCellVol !== null && <Text style={styles.parameterText}>MinCellVol: {MinCellVol.toFixed(4)} V</Text>}
+        {AvgCellVol !== null && <Text style={styles.parameterText}>AvgCellVol: {AvgCellVol.toFixed(4)} V</Text>}
+        {MaxVoltId !== null && <Text style={styles.parameterText}>MaxVoltId: {MaxVoltId.toFixed(4)} V</Text>}
+        {MinVoltId !== null && <Text style={styles.parameterText}>MinVoltId: {MinVoltId.toFixed(4)} V</Text>}
         {cellVol01 === null && cellVol02 === null && cellVol03 === null && cellVol04 === null && 
        cellVol05 === null && cellVol06 === null && cellVol07 === null && cellVol08 === null && 
        cellVol09 === null && cellVol10 === null && cellVol11 === null && cellVol12 === null && 
        cellVol13 === null && cellVol14 === null && cellVol15 === null && cellVol16 === null && 
+       MaxCellVol === null && MinCellVol === null && AvgCellVol === null && MaxVoltId === null && 
+       cellVol01 === null && 
  <Text>No Data Received Yet</Text>}
       </View>
     </ScrollView>
@@ -149,7 +175,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
-  cellVolText: {
+  parameterText: {
     color: '#FFA500',
     fontSize: 20,
     fontWeight: 'bold',
